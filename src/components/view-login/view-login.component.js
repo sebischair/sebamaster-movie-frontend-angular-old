ca9f0059bf5d58451439dec9720a23eaafb41ada
Adrian Hernandez-Mendez <adrian.hernandez@tum.de>
2017-04-30 12:39:02 +0200

    Another details

--- src/components/view-login/view-login.component.js
+++ src/components/view-login/view-login.component.js
@@ -1,6 +1,8 @@
 
 'use strict';
 
+import UserService from './../../services/user/user.service';
+
 import template from './view-login.template.html';
 import './view-login.style.css';
 
@@ -19,8 +21,26 @@ class ViewLoginComponent {
 }
 
 class ViewLoginComponentController{
-    constructor(){
+    constructor($state,UserService){
+        this.$state = $state;
+        this.UserService = UserService;
+    }
 
+    $onInit() {
+        this.login = {};
+    }
+
+    submit(){
+        let user = this.login.user;
+        let password = this.login.password;
+
+        this.UserService.login(user,password).then(()=> {
+            this.$state.go('movies',{});
+        });
+    }
+
+    static get $inject(){
+        return ['$state', UserService.name];
     }
 
 }
